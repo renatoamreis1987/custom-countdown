d86985e9e1d4cc3e33ed6e7f20a807684c268e69
Renato Reis <email@renatoamreis.com>
2021-02-14 20:58:05 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,3 @@
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdownForm");
+const dateEl = document.getElementById("date-picker");
